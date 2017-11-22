e33067ba928d6fa67ab2f366b41727f9debb6e2a
Steven Loria <sloria1@gmail.com>
2017-11-22 17:45:34 -0500

    Update copyright

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
     <footer className="footer">
       <div className="container">
         <p className="text-muted">
-          &copy; 2016 Steven Loria&nbsp;|&nbsp;
+          &copy; 2017 Steven Loria&nbsp;|&nbsp;
           <Link to="/">Home</Link>&nbsp;|&nbsp;
           <a href="http://github.com/sloria/simplist"><FAIcon type="github" /> Source code</a>
         </p>
